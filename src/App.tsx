3cdc1619203bf1b6e083f0ec5c68228cd8db4a4f
Emmanuel Oyeleke <oyelekemmanuel@gmail.com>
2025-07-21 16:38:31 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import programmerIllustration from "./assets/programmer_illustration.png";
+import programmer from "./assets/programmer.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className=" w-full min-h-screen">
+      <nav className="sticky top-0 z-50 bg-white/30 backdrop-blur-md border-b border-white/20">
+        <ul className="flex items-center justify-center gap-10 py-4 ">
+          <li className="font-medium cursor-pointer bg-primary text-white p-2 rounded-lg transition-colors">
+            About Me
+          </li>
+          <li className="font-medium cursor-pointer hover:text-primary transition-colors">
+            Skills
+          </li>
+          <li className="font-medium cursor-pointer hover:text-primary transition-colors">
+            Project
+          </li>
+          <li className="font-medium cursor-pointer hover:text-primary transition-colors">
+            Contact Me
+          </li>
+        </ul>
+      </nav>
+      <div className="flex ">
+        <div className="w-1/2 flex justify-center flex-col px-10">
+          <h2 className="text-6xl font-semibold">
+            Hi,
+            <br /> I'm Emmanuel,{" "}
+          </h2>
+          <p className="text-4xl leading-relaxed mt-2">
+            a seasoned front-end developer proficient in buidling beautiful and
+            efficient user interfaces using React and React Native.
+          </p>
+
+          <div className="flex gap-5 mt-10">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="35"
+              height="35"
+              viewBox="0 0 24 24"
+              className="cursor-pointer fill-black hover:fill-primary transition-all duration-200 hover:scale-120"
+            >
+              <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="35"
+              height="35"
+              viewBox="0 0 24 24"
+              className="cursor-pointer fill-black hover:fill-primary transition-colors duration-200 hover:scale-120"
+            >
+              <path d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="35"
+              height="35"
+              viewBox="0 0 24 24"
+              className="cursor-pointer fill-black hover:fill-primary transition-colors duration-200 hover:scale-120"
+            >
+              <path d="M 5 3 C 3.895 3 3 3.895 3 5 L 3 19 C 3 20.105 3.895 21 5 21 L 19 21 C 20.105 21 21 20.105 21 19 L 21 5 C 21 3.895 20.105 3 19 3 L 5 3 z M 5 5 L 19 5 L 19 19 L 5 19 L 5 5 z M 7.7792969 6.3164062 C 6.9222969 6.3164062 6.4082031 6.8315781 6.4082031 7.5175781 C 6.4082031 8.2035781 6.9223594 8.7167969 7.6933594 8.7167969 C 8.5503594 8.7167969 9.0644531 8.2035781 9.0644531 7.5175781 C 9.0644531 6.8315781 8.5502969 6.3164062 7.7792969 6.3164062 z M 6.4765625 10 L 6.4765625 17 L 9 17 L 9 10 L 6.4765625 10 z M 11.082031 10 L 11.082031 17 L 13.605469 17 L 13.605469 13.173828 C 13.605469 12.034828 14.418109 11.871094 14.662109 11.871094 C 14.906109 11.871094 15.558594 12.115828 15.558594 13.173828 L 15.558594 17 L 18 17 L 18 13.173828 C 18 10.976828 17.023734 10 15.802734 10 C 14.581734 10 13.930469 10.406562 13.605469 10.976562 L 13.605469 10 L 11.082031 10 z"></path>
+            </svg>
+          </div>
+        </div>
+        <img
+          src={programmerIllustration}
+          style={{ width: "800px" }}
+          alt="illustration"
+        />
+      </div>
+
+      <div className="bg-lighter-primary min-h-100"></div>
+    </div>
+  );
+}
+
+export default App;
