4e35f8de258ded0c58278d61de9ac506112fcc02
Emmanuel Oyeleke <oyelekemmanuel@gmail.com>
2025-07-29 12:19:29 +0100

    added some responsiveness and new icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,17 @@
-import "./App.css";
-
-import programmerIllustration from "./assets/programmer_illustration.png";
-import pathwayMockup from "./assets/Pathway-mockup.png";
-
-import Skills from "./components/sections/Skills";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-function App() {
+const Footer = () => {
   return (
-    <div
-      className=" w-full min-h-screen"
-      id="about"
+    <footer
+      className="relative min-h-90 bg-lighter-primary px-5 py-5 md:px-15 md:py-10"
+      id="contact"
     >
-      <Navbar />
-      <div className="flex px-5 md:px-15">
-        <div className=" flex justify-center flex-col md:w-1/2">
-          <div className="text-center mt-15 md:text-left">
-            <h2 className="text-4xl font-semibold md:text-6xl">
-              Hi,
-              <br /> I'm Emmanuel,
-            </h2>
-            <p className="text-xl leading-relaxed mt-2 md:text-4xl">
-              a seasoned front-end developer proficient in buidling beautiful
-              and efficient user interfaces using React and React Native.
-            </p>
-          </div>
-          <div className="flex justify-center gap-5 mt-5 mb-15 md:my-10 sm:justify-start">
+      <div className="text-center md:text-left">
+        <h3 className="text-3xl font-semibold">Emmanuel Oyeleke</h3>
+        <p className="text-xl">Front-end Developer</p>
+      </div>
+
+      <div className="absolute bottom-5 px-5 py-5 md:px-15 md:py-10  flex flex-col items-center justify-center">
+        <div className="flex gap-5 mb-3">
+          <div className="h-12 w-12 flex items-center justify-center bg-light-primary rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               x="0px"
@@ -37,7 +22,8 @@ function App() {
             >
               <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
             </svg>
-
+          </div>
+          <div className="h-12 w-12 flex items-center justify-center bg-light-primary rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               x="0px"
@@ -48,7 +34,8 @@ function App() {
             >
               <path d="M26.37,26l-8.795-12.822l0.015,0.012L25.52,4h-2.65l-6.46,7.48L11.28,4H4.33l8.211,11.971L12.54,15.97L3.88,26h2.65 l7.182-8.322L19.42,26H26.37z M10.23,6l12.34,18h-2.1L8.12,6H10.23z"></path>
             </svg>
-
+          </div>
+          <div className="h-12 w-12 flex items-center justify-center bg-light-primary rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               x="0px"
@@ -66,7 +53,8 @@ function App() {
                 d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
               ></path>
             </svg>
-
+          </div>
+          <div className="h-12 w-12 flex items-center justify-center bg-light-primary rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               x="0px"
@@ -98,44 +86,14 @@ function App() {
             </svg>
           </div>
         </div>
-        <img
-          src={programmerIllustration}
-          style={{ width: "800px" }}
-          className=" hidden md:block"
-          alt="illustration"
-        />
+        <p className="my-3 text-center md:text-left">
+          Feel free to reach out if you have a gig or an opportunity for me!
+        </p>
+        <hr className="border-primary w-1/3 m-auto mb-5" />
+        <p className=" text-center">created with ❤️ by me.</p>
       </div>
-
-      <Skills />
-
-      <div className="bg-white min-h-100 px-5 py-5 md:px-15 md:py-10">
-        <h2
-          className="text-xl font-medium text-center mb-5 md:text-4xl"
-          id="projects"
-        >
-          Projects
-        </h2>
-
-        <div className="grid my-7 gap-10 md:gap-10 md:my-10 md:grid-cols-2">
-          <div className="bg-gray-100 p-5 md:p-10 rounded-xl">
-            <img
-              src={pathwayMockup}
-              alt="UTLAM"
-            />
-          </div>
-
-          <div className="bg-gray-100 p-5 md:p-10 rounded-xl">
-            <img
-              src={pathwayMockup}
-              alt="UTLAM"
-            />
-          </div>
-        </div>
-      </div>
-
-      <Footer />
-    </div>
+    </footer>
   );
-}
+};
 
-export default App;
+export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 bg-white/30 backdrop-blur-md border-b border-white/20">
-      <ul className="flex items-center justify-center gap-10 py-4 ">
+      <ul className="sm:flex items-center justify-center hidden  py-4 gap-4 sm:text-sm md:gap-10 ">
         <li className="font-medium cursor-pointer bg-primary text-white p-2 rounded-lg transition-colors">
           <a href="/#about">About Me</a>
         </li>
@@ -15,6 +15,18 @@ const Navbar = () => {
           <a href="/#contact">Contact Me</a>
         </li>
       </ul>
+      <div className="flex items-center justify-end px-4 py-2 md:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="35"
+          height="35"
+          viewBox="0 0 30 30"
+        >
+          <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
+        </svg>
+      </div>
     </nav>
   );
 };
